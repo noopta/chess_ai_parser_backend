c545ec59fcee3fb7fe411495a0225f5afb8dfbda
Ubuntu <ubuntu@ip-172-31-5-139.us-east-2.compute.internal>
2024-12-31 13:56:54 +0000

    grpc test server and client

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -2,10 +2,16 @@ use std::io::{BufRead, BufReader, Write};
 use std::process::{Command, Stdio};
 
 fn main() -> std::io::Result<()> {
+    
+    get_chess_data();
+    // mutable variable that creates a new child process based on a binary 
+    // Command runs a CLI based command essentially 
     let mut child = Command::new("../Stockfish/src/stockfish")
+    // creates a standard input pipe
         .stdin(Stdio::piped())
+        // creates a standard output pipe 
         .stdout(Stdio::piped())
-        .spawn()?;
+        .spawn()?; // spawns the child process
 
     let mut stdin = child.stdin.take().unwrap();
     let stdout = child.stdout.take().unwrap();
@@ -26,7 +32,11 @@ fn main() -> std::io::Result<()> {
     writeln!(stdin, "go depth 10")?;
 
     reader_thread.join().unwrap();
-
     Ok(())
     
+}
+
+
+fn get_chess_data() {
+    println!("hi")
 }
