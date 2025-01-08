286e08a18c93d4240ad739ca55f4d2512bdb3c86
Ubuntu <ubuntu@ip-172-31-5-139.us-east-2.compute.internal>
2025-01-08 12:57:31 +0000

    retrieving api data

--- hello_cargo/src/client.rs
+++ hello_cargo/src/client.rs
@@ -1,5 +1,7 @@
-use hello_world::greeter_client::GreeterClient;
-use hello_world::HelloRequest;
+// use hello_world::greeter_client::GreeterClient;
+// use hello_world::{HelloRequest, ProfileRequestData};
+use hello_world::{ProfileRequestData};
+use hello_world::chess_service_client::ChessServiceClient;
 
 pub mod hello_world {
     tonic::include_proto!("hello_cargo");
@@ -7,15 +9,47 @@ pub mod hello_world {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    // let mut client = GreeterClient::connect("http://[::1]:50051").await?;
 
-    let request = tonic::Request::new(HelloRequest {
-        name: "Tonic".into(),
+    // let request = tonic::Request::new(HelloRequest {
+    //     name: "Tonic".into(),
+    // });
+
+    // let response = client.say_hello(request).await?;
+
+    // println!("RESPONSE={:?}", response.into_inner().message);
+
+
+    let mut client = ChessServiceClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(ProfileRequestData {
+        username: "noopdogg07".into(),
+        month: "10".into(),
+        year: "2024".into(),
+        number_of_games: "2".into()
     });
 
-    let response = client.say_hello(request).await?;
-
-    println!("RESPONSE={:?}", response.into_inner().message);
+    let response = client.get_chess_games(request).await?;
+    
+    println!("RESPONSE={:?}", response);
 
     Ok(())
 }
+
+
+// async fn test() -> Result<(), Box<dyn std::error::Error>> {
+//     let mut client = ChessServiceClient::connect("http://[::1]:50051").await?;
+
+//     let request = tonic::Request::new(ProfileRequestData {
+//         username: "noopdogg07".into(),
+//         month: "10".into(),
+//         year: "2024".into(),
+//         number_of_games: "2".into()
+//     });
+
+//     let response = client.get_chess_games(request).await?;
+    
+//     println!("RESPONSE={:?}", response);
+
+//     Ok(())
+// }
--- hello_cargo/src/server.rs
+++ hello_cargo/src/server.rs
@@ -1,7 +1,12 @@
 use tonic::{transport::Server, Request, Response, Status};
+use reqwest::header;
+use reqwest::Client;
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
+// use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::greeter_server::{Greeter};
 use hello_world::{HelloReply, HelloRequest};
+use hello_world::chess_service_server::{ChessService, ChessServiceServer};
+use hello_world::{ProfileRequestData, ChessApiGameResponse, ChessPlayer, Game};
 
 pub mod hello_world {
     tonic::include_proto!("hello_cargo");
@@ -9,6 +14,156 @@ pub mod hello_world {
 
 #[derive(Debug, Default)]
 pub struct MyGreeter {}
+
+#[derive(Debug, Default)]
+pub struct ChessStruct{}
+
+#[derive(serde::Deserialize, Debug)]
+struct LocalChessApiGameResponse {
+    games: Vec<LocalGame>
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct LocalGame {
+    url: String,
+    pgn: String,
+    tcn: String,
+    fen: String,
+    white: LocalChessPlayer,
+    black: LocalChessPlayer
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct LocalChessPlayer {
+    rating: i32,
+    result: String,
+    username: String
+}
+
+impl LocalChessApiGameResponse {
+    fn into_proto(self) -> hello_world::ChessApiGameResponse {
+        hello_world::ChessApiGameResponse {
+            // map local games into proto games
+            games: self.games.into_iter().map(|g| g.into_proto()).collect(),
+        }
+    }
+}
+
+impl LocalGame {
+    fn into_proto(self) -> hello_world::Game {
+        hello_world::Game {
+            url: self.url,
+            pgn: self.pgn,
+            tcn: self.tcn,
+            fen: self.fen,
+            white: Some(self.white.into_proto()),
+            black: Some(self.black.into_proto()),
+        }
+    }
+}
+
+impl LocalChessPlayer {
+    fn into_proto(self) -> hello_world::ChessPlayer {
+        hello_world::ChessPlayer {
+            rating: self.rating,
+            result: self.result,
+            username: self.username,
+        }
+    }
+}
+
+// fn convert_game(current_game: ChessApiGameResponse) -> hello_world::Game {
+//     hello_world::Game {
+//         url: current_game.url,
+//         pgn: current_game.pgn,
+//         tcn: current_game.tcn,
+//         fen: current_game.fen,
+//         white: Some(hello_world::Player {
+//             rating: current_game.white.rating,
+//             result: current_game.white.result,
+//             username: current_game.white.username
+//         }),
+//         black: Some(hello_world::Player {
+//             rating: current_game.black.rating,
+//             result: current_game.black.result,
+//             username: current_game.black.username
+//         }),
+//     }
+// }
+
+pub async fn fetch_games(username: &str, month: &str, year: &str) -> Result<LocalChessApiGameResponse, Box<dyn std::error::Error>> {
+    let _endpoint_url: String = format!(
+        "https://api.chess.com/pub/player/{}/games/{}/{}",
+        username, year, month
+    );
+
+   // Build a client so we can add headers
+   let client = Client::new();
+   let response = client
+       .get(&_endpoint_url)
+       // Add a custom user agent
+       .header(header::USER_AGENT, "wallace.dev@proton.me")
+       .send()
+       .await?;
+
+    println!("ENDPOINT: {}", _endpoint_url);
+
+    // let response_text = reqwest::get(&_endpoint_url).await?;
+
+    // let chess_games: Vec<ChessApiGameResponse> = serde_json::from_str(&response_text)?;
+    // let parsed_games: Vec<Game> = chess_games.into_iter().map(convert_game).collect();
+    
+    if response.status().is_success() {
+        let json_response: LocalChessApiGameResponse = response.json().await?;
+        println!("RAW JSON: {:?}", json_response);
+        Ok(json_response)
+    } else {
+        println!("HTTP REQUEST FAILED WITH SSTATUS: {}", response.status());
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "HTTP REQUEST FAILED",
+        )))
+    }
+
+    // for (idx, game) in parsed_games.iter().enumerate() {
+    //     println!("game: {} index: {}", game.url, idx);
+    // }
+
+    // Ok(ChessApiResponse{games: parsed_games})
+}
+
+#[tonic::async_trait]
+impl ChessService for ChessStruct {
+    async fn get_chess_games(
+        &self,
+        request: Request<ProfileRequestData>,
+    ) -> Result<Response<hello_world::ChessApiGameResponse >, Status> {
+        let request_ref = request.get_ref().clone();
+        println!("Chess request {:?}", request_ref);
+        
+        // int rating = 1;
+        // string result = 2;
+        // string username = 3;
+        let content = request.get_ref();
+        // let innerContent = request.into_inner().clone();
+
+        let username = &content.username;
+        let month = &content.month;
+        let year = &content.year;
+
+        match fetch_games(username, month, year).await {
+            Ok(chess_response) => {
+                println!("Fetched chess response: {:?}", chess_response); // Print the raw fetched data
+
+                let proto_response = chess_response.into_proto();
+                Ok(Response::new(proto_response))
+            },
+            Err(e) => Err(Status::internal(format!("Error fetching games: {}", e))),
+        }
+        // https://api.chess.com/pub/player/noopdogg07/games/2024/010
+        // Ok(Response::new(reply))
+    }
+}
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
@@ -29,12 +184,24 @@ impl Greeter for MyGreeter {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::default();
+    // let greeter = MyGreeter::default();
+
+    let chess_service = ChessStruct::default();
+
+    // Server::builder()
+    //     .add_service(GreeterServer::new(greeter))
+    //     .serve(addr)
+    //     .await?;
+
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
-        .serve(addr)
-        .await?;
+    .add_service(ChessServiceServer::new(chess_service))
+    .serve(addr)
+    .await?;
 
     Ok(())
 }
+
+// api link
+
+// "https://api.chess.com/pub/player/noopdogg07/games/2024/010
